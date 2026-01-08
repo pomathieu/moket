f584b8356fa3de2d3e487bcb9178a28d4cac44b5
pomathieu <po.mathieu@thetradecopilot.com>
2026-01-08 17:34:20 +0100

    maj

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Toaster } from 'sonner';
 import { Analytics } from '@vercel/analytics/next';
 import './globals.css';
 import Script from 'next/script';
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 
 const inter = Inter({
   subsets: ['latin'],
@@ -144,8 +145,33 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       lang="fr"
       suppressHydrationWarning
       className={`${inter.variable} ${sora.variable}`}>
+      <head>
+        {GTM_ID && (
+          <Script
+            id="gtm-head"
+            strategy="afterInteractive">
+            {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `}
+          </Script>
+        )}
+      </head>
       <body className="min-h-dvh bg-background text-foreground antialiased">
         {/* dans le JSX */}
+        {GTM_ID && (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
+        )}
         <Script
           id="ld-localbusiness"
           type="application/ld+json"
